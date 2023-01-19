6d03ef6d229c8f5d5875ea4954f11056100abbb4
OksanaTryhub <ksanatrigub@gmail.com>
2023-01-19 22:53:22 +0200

    update

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import FeedbackWidget from './components/FeedbackWidget/FeedbackWidget';
+// import css from './App.module.css';
+
+export default function App() {
+  return (
+    <>
+      <FeedbackWidget />
+    </>
+  );
+}
--- src/components/App.jsx
+++
@@ -1,9 +0,0 @@
-import FeedbackWidget from '../components/FeedbackWidget/FeedbackWidget';
-
-export const App = () => {
-  return (
-    <div>
-      <FeedbackWidget />
-    </div>
-  );
-};
